d0fdd795f0727b1514b13515667ec224ed44231a
Amol Khedekar <amlu9421@gmail.com>
2022-04-04 01:14:20 +0530

    ADD : Patient module -  OTP Verification and Patient Profile

---
+++ patient/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import index, get_otp, verify_otp, save_patient_profile
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('get-otp/', get_otp, name='get-otp'),
+    path('verify-otp/', verify_otp, name='verify-otp'),
+    path('save-patient-profile/', save_patient_profile,
+         name='save-patient-profile'),
+]
